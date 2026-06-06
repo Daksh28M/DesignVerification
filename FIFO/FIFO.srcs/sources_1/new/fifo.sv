f9b44f58ce32851ca9feb921cc7b5aa35a2483ac
Daksh28M <daksh6382@gmail.com>
2026-06-06 12:03:33 +0530

    Started FIFO Verification

---
+++ FIFO/FIFO.srcs/sources_1/new/fifo.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.06.2026 12:02:50
+// Design Name: 
+// Module Name: fifo
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fifo(
+
+    );
+endmodule
